867d22a67e76fe77ff93142ecef26be571a04fa4
Tomasz Straszewski <tomek.straszewski@gmail.com>
2023-09-15 20:11:30 +0100

    Tweaks physic

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -14,6 +14,13 @@ public class GameMode : MonoBehaviour
     public GameObject craft;
     public Transform spawnPoint;
     public TMP_Text distanceText;
+    public GameObject finishText;
+
+    float stationaryTimer = 0.0f;
+    bool finish = false;
+    bool start = false;
+    float distanceTravelled = 0.0f;
+    float lastDistanceTravelled = 0.0f;
 
     public float startingForce = -3200.0f;
 
@@ -65,10 +72,20 @@ public class GameMode : MonoBehaviour
         // start simulation
         if (Input.GetButtonDown("Fire1"))
         {
+            start = true;
             Time.timeScale = 1.0f;
         }
 
-        UpdateDistanceTravelled();
+        if (finish && Input.GetButtonDown("Fire1"))
+        {
+            // need first clear the level
+            finish = false;
+            distanceTravelled = 0.0f;
+            craftScript.CraftBuild.Clear();
+            DestroyImmediate(craft);
+            SceneManager.LoadScene("DesignScene", LoadSceneMode.Single);
+        }
+
     }
 
     public void FixedUpdate()
@@ -80,13 +97,39 @@ public class GameMode : MonoBehaviour
             Vector3 m_Direction = Vector3.forward;
             baseRB.AddForce(m_Direction * startingForce);
         }
+
+        // need to do in fixed update because of finish detecting 
+        UpdateDistanceTravelled();
+        if (finish)
+        {
+            Debug.Log("FINISHED!");
+            finishText.SetActive(true);
+
+        }
+
     }
 
     public void UpdateDistanceTravelled()
     {
-        float distanceTravelled = spawnPoint.position.z - baseRB.transform.position.z;
+        distanceTravelled = spawnPoint.position.z - baseRB.transform.position.z;
         distanceTravelled = Mathf.Max(0, distanceTravelled);
         distanceText.text = "Distance: " + distanceTravelled.ToString("F1") + "m";
+        if(start)
+        {
+            finish = detectCraftStopped();
+        }
+        lastDistanceTravelled = distanceTravelled;
+        Debug.Log("LastDistance: " + lastDistanceTravelled);
+        Debug.Log("Distance: " + distanceTravelled);
+    }
+
+    public bool detectCraftStopped()
+    { 
+        if((distanceTravelled - lastDistanceTravelled < 0.01))
+        {
+            stationaryTimer += Time.deltaTime;
+        }
+        return (stationaryTimer > 5.0f);
     }
 
 }
